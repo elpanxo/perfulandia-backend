bc393e8fba9ab87ba771dc8645dee039a0ce2e78
1tokkio <matiasvargas124@gmail.com>
2025-05-24 10:09:02 -0400

    commit

--- notificationservice/src/main/java/com/perfulandia/notificationservice/controller/NotificationController.java
+++ notificationservice/src/main/java/com/perfulandia/notificationservice/controller/NotificationController.java
@@ -1,4 +1,23 @@
 package com.perfulandia.notificationservice.controller;
 
+import com.perfulandia.notificationservice.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/notificaciones")
 public class NotificationController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping
+    public String enviarCorreo(@RequestBody NotificationRequest request) {
+        emailService.enviarCorreo(
+                request.getDestinatario(),
+                request.getAsunto(),
+                request.getMensaje()
+        );
+        return "Correo enviado a " + request.getDestinatario();
+    }
 }
--- notificationservice/src/main/java/com/perfulandia/notificationservice/controller/NotificationRequest.java
+++ notificationservice/src/main/java/com/perfulandia/notificationservice/controller/NotificationRequest.java
@@ -1,23 +1,4 @@
 package com.perfulandia.notificationservice.controller;
 
-import com.perfulandia.notificationservice.service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/notificationservice")
-public class NotificationController {
-
-    @Autowired
-    private EmailService emailService;
-
-    @PostMapping
-    public String enviarCorreo(@RequestBody NotificacionRequest request) {
-        emailService.enviarCorreo(
-                request.getDestinatario(),
-                request.getAsunto(),
-                request.getMensaje()
-        );
-        return "Correo enviado a " + request.getDestinatario();
-    }
+public class NotificationRequest {
 }
